c33d061102577f387cb1d98e3fe062aba795a024
Christopher <chantrex@hotmail.com>
2023-03-13 21:31:09 -0400

    Creating the projec NavBar

--- hello_world/src/App.js
+++ hello_world/src/App.js
@@ -1,5 +1,13 @@
 function App() {
   return (
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
       <h1>Hello World</h1>
       <h2>Hello World</h2>
       <h3>Hello World</h3>
